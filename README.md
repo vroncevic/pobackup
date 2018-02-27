248751cb454ee839079cfa7bea06852ef29b6055
Vladimir Roncevic <elektron.ronca@gmail.com>
2018-02-27 06:42:44 +0100

    [pobackup] Updated calls from sh_util, updated README.md

--- README.md
+++ README.md
@@ -1,35 +1,32 @@
-Backup mechanism Postgres DB (Bash scripts).
-================================================================================
+# Backup mechanism Postgres DB (Bash scripts).
 
 The README is used to introduce the tool and provide instructions on
 how to install the tool, any machine dependencies it may have and any
 other information that should be provided before the tool is installed.
 
-INSTALLATION
+### INSTALLATION
 
 To install this tool type the following:
 
-	cp -R ~/pobackup/bin/   /root/scripts/pobackup/ver.1.0/
+```
+cp -R ~/pobackup/bin/   /root/scripts/pobackup/ver.1.0/
+cp -R ~/pobackup/conf/  /root/scripts/pobackup/ver.1.0/
+cp -R ~/pobackup/log/   /root/scripts/pobackup/ver.1.0/
+```
 
-	cp -R ~/pobackup/conf/  /root/scripts/pobackup/ver.1.0/
-
-	cp -R ~/pobackup/log/   /root/scripts/pobackup/ver.1.0/
-
-
-DEPENDENCIES
+### DEPENDENCIES
 
 This tool requires these other modules and libraries:
 
-	sh_util https://github.com/vroncevic/sh_util
+	* sh_util https://github.com/vroncevic/sh_util
 
-COPYRIGHT AND LICENCE
+### COPYRIGHT AND LICENCE
 
-Copyright (C) 2016 by https://github.com/vroncevic/pobackup
+Copyright (C) 2018 by https://vroncevic.github.io/pobackup
 
 This tool is free software; you can redistribute it and/or modify
 it under the same terms as Bash itself, either Bash version 4.2.47 or,
 at your option, any later version of Bash 4 you may have available.
 
-![alt tag](https://raw.githubusercontent.com/vroncevic/pobackup/master/bash_logo.png)
-![alt tag](https://raw.githubusercontent.com/vroncevic/pobackup/master/linux_logo.jpg)
+:sparkles:
 
--- bin/pobackup.sh
+++ bin/pobackup.sh
@@ -11,14 +11,14 @@ UTIL_VERSION=ver.1.0
 UTIL=${UTIL_ROOT}/sh_util/${UTIL_VERSION}
 UTIL_LOG=${UTIL}/log
 
-.	${UTIL}/bin/devel.sh
-.	${UTIL}/bin/usage.sh
-.	${UTIL}/bin/check_root.sh
-.	${UTIL}/bin/check_tool.sh
-.	${UTIL}/bin/logging.sh
-.	${UTIL}/bin/load_conf.sh
-.	${UTIL}/bin/load_util_conf.sh
-.	${UTIL}/bin/progress_bar.sh
+.    ${UTIL}/bin/devel.sh
+.    ${UTIL}/bin/usage.sh
+.    ${UTIL}/bin/check_root.sh
+.    ${UTIL}/bin/check_tool.sh
+.    ${UTIL}/bin/logging.sh
+.    ${UTIL}/bin/load_conf.sh
+.    ${UTIL}/bin/load_util_conf.sh
+.    ${UTIL}/bin/progress_bar.sh
 
 POBACKUP_TOOL=pobackup
 POBACKUP_VERSION=ver.1.0
@@ -28,23 +28,23 @@ POBACKUP_UTIL_CFG=${POBACKUP_HOME}/conf/${POBACKUP_TOOL}_util.cfg
 POBACKUP_LOG=${POBACKUP_HOME}/log
 
 declare -A POBACKUP_USAGE=(
-	[USAGE_TOOL]="${POBACKUP_TOOL}"
-	[USAGE_ARG1]="[OPTION] help (optional)"
-	[USAGE_EX_PRE]="# Postgres backup mechanism"
-	[USAGE_EX]="${POBACKUP_TOOL} help"
+    [USAGE_TOOL]="${POBACKUP_TOOL}"
+    [USAGE_ARG1]="[OPTION] help (optional)"
+    [USAGE_EX_PRE]="# Postgres backup mechanism"
+    [USAGE_EX]="${POBACKUP_TOOL} help"
 )
 
 declare -A POBACKUP_LOGGING=(
-	[LOG_TOOL]="${POBACKUP_TOOL}"
-	[LOG_FLAG]="info"
-	[LOG_PATH]="${POBACKUP_LOG}"
-	[LOG_MSGE]="None"
+    [LOG_TOOL]="${POBACKUP_TOOL}"
+    [LOG_FLAG]="info"
+    [LOG_PATH]="${POBACKUP_LOG}"
+    [LOG_MSGE]="None"
 )
 
 declare -A PB_STRUCTURE=(
-	[BW]=50
-	[MP]=100
-	[SLEEP]=0.01
+    [BW]=50
+    [MP]=100
+    [SLEEP]=0.01
 )
 
 TOOL_DBG="false"
@@ -55,100 +55,102 @@ TOOL_NOTIFY="false"
 # @brief   Main entry point
 # @param   Value optional help
 # @retval  Function __pobackup exit with integer value
-#			0   - tool finished with success operation
-#			128 - failed to load tool script configuration from files
-#			129 - missing external tool pgdump
+#            0   - tool finished with success operation
+#            128 - failed to load tool script configuration from files
+#            129 - missing external tool pgdump
 #
-function __pobackup() {
-	local HELP=$1
-	if [ "${HELP}" == "help" ]; then
-		__usage POBACKUP_USAGE
-	fi
-	local FUNC=${FUNCNAME[0]} MSG="None" STATUS_CONF STATUS_CONF_UTIL STATUS
-	MSG="Loading basic and util configuration!"
-	__info_debug_message "$MSG" "$FUNC" "$POBACKUP_TOOL"
-	__progress_bar PB_STRUCTURE
-	declare -A config_pobackup=()
-	__load_conf "$POBACKUP_CFG" config_pobackup
-	STATUS_CONF=$?
-	declare -A config_pobackup_util=()
-	__load_util_conf "$POBACKUP_UTIL_CFG" config_pobackup_util
-	STATUS_CONF_UTIL=$?
-	declare -A STATUS_STRUCTURE=([1]=$STATUS_CONF [2]=$STATUS_CONF_UTIL)
-	__check_status STATUS_STRUCTURE
-	STATUS=$?
-	if [ $STATUS -eq $NOT_SUCCESS ]; then
-		MSG="Force exit!"
-		__info_debug_message_end "$MSG" "$FUNC" "$POBACKUP_TOOL"
-		exit 128
-	fi
-	TOOL_DBG=${config_pobackup[DEBUGGING]}
-	TOOL_LOG=${config_pobackup[LOGGING]}
-	TOOL_NOTIFY=${config_pobackup[EMAILING]}
-	local PGDUMP=${config_pobackup_util[PGDUMP]}
-	__check_tool "${PGDUMP}"
-	STATUS=$?
-	if [ $STATUS -eq $NOT_SUCCESS ]; then
-		MSG="Install tool ${PGDUMP}"
-		__info_debug_message "$MSG" "$FUNC" "$POBACKUP_TOOL"
-		MSG="Force exit!"
-		__info_debug_message_end "$MSG" "$FUNC" "$POBACKUP_TOOL"
-		exit 129
-	fi
-	local COMPANY=${config_pobackup_util[COMPANY]} I
-	local FILE=${COMPANY}.postgres.`date +"%Y%m%d"`
-	local DB=${config_pobackup_util[DB]} UN=${config_pobackup_util[UNAME]}
-	local PGCMD=${config_pobackup_util[PG_CMD]}
-	local PGSQL=${config_pobackup_util[PGSQL]}
-	local PGDIR=${config_pobackup_util[PGDUMP_ROOT_LOCATION]}
-	local DATABASES=${config_atmanager_util[POSTGRES_DATABASES]}
-	IFS=' ' read -ra databases <<< "${DATABASES}"
-	for I in "${databases[@]}"
-	do
-		DB=${I}
-		local CHECKDB="psql -tAc \"${PGCMD}='${DB}'\" > ${DB}.rpt; exit 0"
-		su - postgres -c "${CHECKDB}"
-		local RESULT=$(cat "${PGSQL}/${DB}.rpt")
-		if [ "${RESULT}" == "1" ]; then
-			local SQL="${PGDUMP} ${UN} ${DB} > ${FILE}_${DB}; exit 0"
-			su - postgres -c "$SQL"
-			mv "${PGSQL}/${FILE}_${DB}" "${PGDIR}/"
-			rm "${PGSQL}/${DB}.rpt"
-			local PG_BACKUP="${PGDIR}/${FILE}_${DB}"
-			if [ -e "${PG_BACKUP}" ]; then
-				gzip "${PG_BACKUP}"
-				MSG="Backup [${PG_BACKUP}.gz] was created"
-				__info_debug_message "$MSG" "$FUNC" "$POBACKUP_TOOL"
-				ls -l "${PG_BACKUP}.gz"
-				POBACKUP_LOGGING[LOG_FLAG]="info"
-				POBACKUP_LOGGING[LOG_MSGE]=$MSG
-				__logging POBACKUP_LOGGING
-			fi
-			MSG="Check file [${PG_BACKUP}]"
-			__info_debug_message "$MSG" "$FUNC" "$POBACKUP_TOOL"
-		else
-			MSG="Database [${DB}] does not exist"
-			__info_debug_message "$MSG" "$FUNC" "$POBACKUP_TOOL"
-		fi
-		done
-	__info_debug_message_end "Done" "$FUNC" "$POBACKUP_TOOL"
-	exit 0
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#
+function __pobackup {
+    local HELP=$1
+    if [ "${HELP}" == "help" ]; then
+        usage POBACKUP_USAGE
+    fi
+    local FUNC=${FUNCNAME[0]} MSG="None" STATUS_CONF STATUS_CONF_UTIL STATUS
+    MSG="Loading basic and util configuration!"
+    info_debug_message "$MSG" "$FUNC" "$POBACKUP_TOOL"
+    progress_bar PB_STRUCTURE
+    declare -A config_pobackup=()
+    load_conf "$POBACKUP_CFG" config_pobackup
+    STATUS_CONF=$?
+    declare -A config_pobackup_util=()
+    load_util_conf "$POBACKUP_UTIL_CFG" config_pobackup_util
+    STATUS_CONF_UTIL=$?
+    declare -A STATUS_STRUCTURE=([1]=$STATUS_CONF [2]=$STATUS_CONF_UTIL)
+    check_status STATUS_STRUCTURE
+    STATUS=$?
+    if [ $STATUS -eq $NOT_SUCCESS ]; then
+        MSG="Force exit!"
+        info_debug_message_end "$MSG" "$FUNC" "$POBACKUP_TOOL"
+        exit 128
+    fi
+    TOOL_DBG=${config_pobackup[DEBUGGING]}
+    TOOL_LOG=${config_pobackup[LOGGING]}
+    TOOL_NOTIFY=${config_pobackup[EMAILING]}
+    local PGDUMP=${config_pobackup_util[PGDUMP]}
+    check_tool "${PGDUMP}"
+    STATUS=$?
+    if [ $STATUS -eq $NOT_SUCCESS ]; then
+        MSG="Install tool ${PGDUMP}"
+        info_debug_message "$MSG" "$FUNC" "$POBACKUP_TOOL"
+        MSG="Force exit!"
+        info_debug_message_end "$MSG" "$FUNC" "$POBACKUP_TOOL"
+        exit 129
+    fi
+    local COMPANY=${config_pobackup_util[COMPANY]} I
+    local FILE=${COMPANY}.postgres.`date +"%Y%m%d"`
+    local DB=${config_pobackup_util[DB]} UN=${config_pobackup_util[UNAME]}
+    local PGCMD=${config_pobackup_util[PG_CMD]}
+    local PGSQL=${config_pobackup_util[PGSQL]}
+    local PGDIR=${config_pobackup_util[PGDUMP_ROOT_LOCATION]}
+    local DATABASES=${config_atmanager_util[POSTGRES_DATABASES]}
+    IFS=' ' read -ra databases <<< "${DATABASES}"
+    for I in "${databases[@]}"
+    do
+        DB=${I}
+        local CHECKDB="psql -tAc \"${PGCMD}='${DB}'\" > ${DB}.rpt; exit 0"
+        su - postgres -c "${CHECKDB}"
+        local RESULT=$(cat "${PGSQL}/${DB}.rpt")
+        if [ "${RESULT}" == "1" ]; then
+            local SQL="${PGDUMP} ${UN} ${DB} > ${FILE}_${DB}; exit 0"
+            su - postgres -c "$SQL"
+            mv "${PGSQL}/${FILE}_${DB}" "${PGDIR}/"
+            rm "${PGSQL}/${DB}.rpt"
+            local PG_BACKUP="${PGDIR}/${FILE}_${DB}"
+            if [ -e "${PG_BACKUP}" ]; then
+                gzip "${PG_BACKUP}"
+                MSG="Backup [${PG_BACKUP}.gz] was created"
+                info_debug_message "$MSG" "$FUNC" "$POBACKUP_TOOL"
+                ls -l "${PG_BACKUP}.gz"
+                POBACKUP_LOGGING[LOG_FLAG]="info"
+                POBACKUP_LOGGING[LOG_MSGE]=$MSG
+                logging POBACKUP_LOGGING
+            fi
+            MSG="Check file [${PG_BACKUP}]"
+            info_debug_message "$MSG" "$FUNC" "$POBACKUP_TOOL"
+        else
+            MSG="Database [${DB}] does not exist"
+            info_debug_message "$MSG" "$FUNC" "$POBACKUP_TOOL"
+        fi
+        done
+    info_debug_message_end "Done" "$FUNC" "$POBACKUP_TOOL"
+    exit 0
 }
 
 #
 # @brief   Main entry point
 # @param   Value optional help
 # @exitval Script tool pobackup exit with integer value
-#			0   - tool finished with success operation
-#			127 - run tool script as root user from cli
-#			128 - failed to load tool script configuration from files
-#			129 - missing external tool pgdump
+#            0   - tool finished with success operation
+#            127 - run tool script as root user from cli
+#            128 - failed to load tool script configuration from files
+#            129 - missing external tool pgdump
 #
 printf "\n%s\n%s\n\n" "${POBACKUP_TOOL} ${POBACKUP_VERSION}" "`date`"
-__check_root
+check_root
 STATUS=$?
 if [ $STATUS -eq $SUCCESS ]; then
-	__pobackup $1
+    __pobackup $1
 fi
 
 exit 127
